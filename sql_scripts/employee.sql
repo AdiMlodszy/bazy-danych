0c8636f62e936ea0dc289a3c1fc1f03e9c6eb5a7
addktiv <adriankasprzakschool@gmail.com>
2024-11-03 19:59:46 +0100

    Feat: employee table definition

---
+++ sql_scripts/employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Pracownicy (
+    Numer INTEGER PRIMARY KEY,
+    Imie Varchar(20),
+    Nazwisko Varchar(30),
+    Stanowisko Varchar(30),
+    Pensja Decimal (10, 2),
+    Miasto Varchar(30) 
+);
